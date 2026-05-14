46bfe9e95f92de5dfef19e29d54d8327836aa367
Ondrej-Dufek <ondrejdufek7@gmail.com>
2026-05-14 13:30:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,4 +52,4 @@ app.get('/summoner/:puuid', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
